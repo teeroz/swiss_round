d7cbcfd3555a87903b956f299ef51bc17f11e0fd
Jonghun Shin <jonghun.shin@linecorp.com>
2018-01-04 00:20:44 +0900

    가족 설정 화면도 로딩 적용

--- vue/src/containers/PlayerEditFamilyPage.vue
+++ vue/src/containers/PlayerEditFamilyPage.vue
@@ -3,34 +3,36 @@
     <the-navbar :title="thisPlayer.name + '의 가족설정'">
     </the-navbar>
 
-    <div class="container form-group">
-      <div class="card mt-3">
-        <ul class="list-group list-group-flush">
-          <li 
-            v-for="player in players"
-            :key="player.id"
-            class="list-group-item align-items-center"
-            >
-            <div class="form-check ml-4 mb-0">
-              <input 
-                type="checkbox" 
-                class="form-check-input" 
-                style="margin-top: .4rem!important" 
-                :id="'checkbox_' + player.id"
-                :value="player.id"
-                v-model="familyMembers"
-                >
-              <label class="form-check-label pl-1" :for="'checkbox_' + player.id">{{ player.name }}</label>
-            </div>
-          </li>
-        </ul>
-      </div>
+    <the-loading ref="loading">
+      <div class="container form-group">
+        <div class="card mt-3">
+          <ul class="list-group list-group-flush">
+            <li 
+              v-for="player in players"
+              :key="player.id"
+              class="list-group-item align-items-center"
+              >
+              <div class="form-check ml-4 mb-0">
+                <input 
+                  type="checkbox" 
+                  class="form-check-input" 
+                  style="margin-top: .4rem!important" 
+                  :id="'checkbox_' + player.id"
+                  :value="player.id"
+                  v-model="familyMembers"
+                  >
+                <label class="form-check-label pl-1" :for="'checkbox_' + player.id">{{ player.name }}</label>
+              </div>
+            </li>
+          </ul>
+        </div>
 
-      <div class="d-flex justify-content-end mt-3">
-        <button type="button" class="btn btn-secondary col-2 mx-2" @click="cancel">취소</button>
-        <button type="submit" class="btn btn-primary col-2" @click="submit">확인</button>
+        <div class="d-flex justify-content-end mt-3">
+          <button type="button" class="btn btn-secondary col-2 mx-2" @click="cancel">취소</button>
+          <button type="submit" class="btn btn-primary col-2" @click="submit">확인</button>
+        </div>
       </div>
-    </div>
+    </the-loading>
   </div>
 </template>
 
@@ -57,6 +59,7 @@ export default {
             const players = res.data.players.filter(player => player.id !== this.thisPlayer.id)
             players.sort((a, b) => (a.name > b.name ? 1 : -1))
             this.players = players
+            this.$refs.loading.stop()
           })
       })
   },
@@ -67,6 +70,7 @@ export default {
     },
 
     submit: function () {
+      this.$refs.loading.start()
       this.$axios.swiss.put(`league/${this.$route.params.league_id}/player/${this.$route.params.player_id}`,
                             { 'family': this.familyMembers })
         .then(res => this.$router.go(-1))
